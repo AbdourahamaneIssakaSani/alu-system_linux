b2558073b8c54649e8e1e39338f2a10dc6f009ab
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-03-13 08:52:45 -0700

    :robot: automated commit of 2-python.c

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -18,37 +18,19 @@ void print_python_bytes(PyObject *p)
 	{
 		printf("  size: %ld\n", pb->ob_base.ob_size);
 		printf("  trying string: %s\n", pb->ob_sval);
-		printf("  first %ld bytes: ", pb->ob_base.ob_size);
-		for (Py_ssize_t i = 0; i < pb->ob_base.ob_size && i < 10; ++i)
+		if (pb->ob_base.ob_size < 10)
+			printf("  first %ld bytes: %s\n", pb->ob_base.ob_size + 1, pb->ob_sval);
+		else
 		{
-			printf("%02hhx ", pb->ob_sval[i]);
+			for (Py_ssize_t i = 0; i < pb->ob_base.ob_size && i < 10; ++i)
+			{
+				printf("%02hhx ", pb->ob_sval[i]);
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 	else
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
-	}
-}
-
-
-/**
-* print_python_list - prints some basic info about Python lists
-* @p: Python Object
-*
-* Return: void
-*/
-void print_python_list(PyObject *p)
-{
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-	for (int i = 0; i < Py_SIZE(p); i++)
-
-	{
-		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-		if (PyBytes_Check(PyList_GetItem(p, i)))
-			print_python_bytes(PyList_GetItem(p, i));
 	}
 }

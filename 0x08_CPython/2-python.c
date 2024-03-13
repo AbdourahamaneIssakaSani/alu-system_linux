118272407634d106a75e2bd9de90816f5ee8c54b
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-03-13 08:54:21 -0700

    :robot: automated commit of 2-python.c

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -34,3 +34,24 @@ void print_python_bytes(PyObject *p)
 		printf("  [ERROR] Invalid Bytes Object\n");
 	}
 }
+
+/**
+* print_python_list - prints some basic info about Python lists
+* @p: Python Object
+*
+* Return: void
+*/
+void print_python_list(PyObject *p)
+{
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (int i = 0; i < Py_SIZE(p); i++)
+
+	{
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+		if (PyBytes_Check(PyList_GetItem(p, i)))
+			print_python_bytes(PyList_GetItem(p, i));
+	}
+}

bfd863bfac1a20e2c609ba22a9781db91165be54
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-03-28 04:26:22 -0700

    more to the nonsense

--- multithreading/multithreading.h
+++ multithreading/multithreading.h
@@ -119,5 +119,6 @@ typedef struct task_s
 void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
 void blur_portion(blur_portion_t const *portion);
+void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 #endif /*MULTITHREADING_H*/
 
